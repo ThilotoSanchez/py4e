23f511949f241ad1d24e8db81ff019bac3e1a799
ThilotoSanchez <thilo.schinke@gmail.com>
2021-04-07 18:20:08 +0200

    Completed first exercise in databases.

---
+++ ex_15_databases/ex_15.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect('orgdb.sqlite')
+cur = conn.cursor()
+
+# emptying table evey time code runs
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+# set up table
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+# loop through file and add org with count to database
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1].split('@')
+    org = email[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
